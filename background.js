cb40892401b5458f4f6cfcdd144ed4fcf156f7a2
Dennis Brown <vilesyn@gmail.com>
2020-05-30 17:51:37 -0500

    add account checking and timing

--- background.js
+++ background.js
@@ -1,32 +1,38 @@
 (function() {
   let nextTime, lastTime, waitTime = 0, pollTime = 15000;
+  const Account = new messageChecker.Account;
+  const Utilities = new messageChecker.Utilities;
   const Preferences = new messageChecker.Preferences;
   const Conversations = new messageChecker.Conversations;
   let convoMembers = messageChecker.setupConversations();
 
   function checkInbox() {
-    Conversations.initList(list => {
-      console.log(list);
+    Utilities.fetchPage(window.baseUrl, page => {
+      Account.checkLoginStatus(page, loginStatus => {
+        if (loginStatus.enabled) {
+          Conversations.initList(() => {
+            badgeUpdate();
+            statusUpdate();
+          });
+        }
+      });
     });
   }
 
   function updateList() {
     let oldTime = waitTime;
-    Preferences.get('waitTime', store => {
-      waitTime = store;
-      if (waitTime === 0) {
-        waitTime = 15;
-      }
+    if (waitTime === 0) {
+      waitTime = 15;
+    }
 
-      if (waitTime !== oldTime) {
-        console.log('reset');
+    if (waitTime !== oldTime) {
+      console.log('reset');
 
-        checkTime = setInterval(updateList, pollTime);
-        lastTime = new Date();
-        nextTime = lastTime.getTime() + waitTime * 60000;
-        checkInbox();
-      }
-    });
+      checkTime = setInterval(updateList, pollTime);
+      lastTime = new Date();
+      nextTime = lastTime.getTime() + waitTime * 60000;
+      checkInbox();
+    }
 
     if (nextTime !== undefined) {
       if (Date.now() >= nextTime) {
@@ -36,20 +42,32 @@
     }
   }
 
-  function _statusUpdate() {
+  function badgeUpdate() {
+    console.log('Update badge');
+    if(convoMembers.total > 0) {
+      chrome.browserAction.setBadgeText({text: convoMembers.total.toString()});
+    }
+    if(convoMembers.total !== 0) {
+      chrome.browserAction.setBadgeBackgroundColor({color: [185,0,0,255]});
+    } else {
+      chrome.browserAction.setBadgeBackgroundColor({color: [125,125,225,255]});
+    }
+  }
+
+  function statusUpdate() {
     console.log('update notification status');
-    Preferences.get('notifications', store => {
-      status = store;
-      if (status && nextTime) {
-        if (convoMembers.total > 0) {
-          new Notification('BlenderMarket Inbox', {
-            icon: chrome.extension.getURL('icon.png'),
-            body: `${Conversations.total.toString()} messages`
-          });
-        }
+    Preferences.get('notifications', isEnabled => {
+      let total = convoMembers.total ? convoMembers.total.toString() : '0';
+      if (isEnabled && nextTime) {
+        new Notification('BlenderMarket Inbox', {
+          icon: chrome.extension.getURL('images/get_started128.png'),
+          body: `${total} messages`
+        });
       }
     });
   }
+
+  checkTime = setInterval(updateList, pollTime);
 
   function start() {
     updateList();
