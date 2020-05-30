f36eb2740e84f1347f1d48c682ccff3f7563006c
Dennis Brown <vilesyn@gmail.com>
2020-05-30 00:01:12 -0500

    update list populator for the popup

--- popup.js
+++ popup.js
@@ -5,11 +5,20 @@
 
   function populateList() {
     Conversations.initList(messages => {
-      console.log(messages);
       let messageList = document.querySelector('.message-list');
       messages.map(message => {
         var messageBlock = document.createElement('div');
-        messageBlock.innerHTML = message.subject;
+        var messageHeader = document.createElement('div');
+        var messageBody = document.createElement('div');
+        var messageImage = document.createElement('img');
+        messageImage.style = message.media;
+        // messageBlock.src = message.link;
+        messageHeader.innerHTML = message.product;
+        messageBody.innerHTML = message.subject;
+        messageBlock.append(messageHeader);
+        messageBlock.append(messageBody);
+        messageBlock.append(messageImage);
+        console.log(messageBlock);
         messageList.append(messageBlock);
       })
     });
