fb55f8db433b38855ba04bca41927f25f1300da2
Dennis Brown <vilesyn@gmail.com>
2020-05-30 13:24:08 -0500

    add classes and separate element building

--- popup.js
+++ popup.js
@@ -1,32 +1,50 @@
 (function() {
   // Popup Content Script
-  var console = chrome.extension.getBackgroundPage().console;
   const Conversations = new messageChecker.Conversations;
+  var console, backgroundPage = chrome.extension.getBackgroundPage();
+
+  if (backgroundPage) {
+    console = backgroundPage.console
+    console.log('Popup loaded');
+  }
+
+  function buildContent(message) {
+    let messageList = document.querySelector('.message-list');
+    let messageBlock = document.createElement('a');
+    messageBlock.setAttribute('target', '_inbox');
+    messageBlock.setAttribute('class', 'message-block');
+
+    let contentBlock = document.createElement('div');
+    contentBlock.setAttribute('class', 'content-block');
+
+    let messageHeader = document.createElement('div');
+    messageHeader.setAttribute('class', 'message-header');
+
+    let messageBody = document.createElement('div');
+    messageBody.setAttribute('class', 'message-body');
+
+    let messageImage = document.createElement('img');
+    messageImage.setAttribute('class', 'message-image');
+    messageImage.style = message.media;
+    messageBlock.href = message.link;
+
+    messageHeader.innerHTML = message.product;
+    messageBody.innerHTML = message.subject;
+    contentBlock.append(messageHeader);
+    contentBlock.append(messageBody);
+    messageBlock.append(messageImage);
+    messageBlock.append(contentBlock);
+
+    messageList.append(messageBlock);
+  }
 
   function populateList() {
     Conversations.initList(messages => {
-      let messageList = document.querySelector('.message-list');
-      messages.map(message => {
-        var messageBlock = document.createElement('div');
-        var messageHeader = document.createElement('div');
-        var messageBody = document.createElement('div');
-        var messageImage = document.createElement('img');
-        messageImage.style = message.media;
-        // messageBlock.src = message.link;
-        messageHeader.innerHTML = message.product;
-        messageBody.innerHTML = message.subject;
-        messageBlock.append(messageHeader);
-        messageBlock.append(messageBody);
-        messageBlock.append(messageImage);
-        console.log(messageBlock);
-        messageList.append(messageBlock);
-      })
+      messages.map(message => buildContent(message))
     });
   }
 
-  console.log('Popup loaded');
   // call object from background here
-
   function start() {
     console.log('Starting Popup ' + chrome.app.getDetails().version);
     populateList();
