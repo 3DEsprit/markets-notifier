0f1e1a2d5e5816045b9f5dde2567cb7f7eaa1de4
Dennis Brown <vilesyn@gmail.com>
2020-05-29 23:27:47 -0500

    fix listpopulating

--- popup.js
+++ popup.js
@@ -1,12 +1,26 @@
 (function() {
   // Popup Content Script
   var console = chrome.extension.getBackgroundPage().console;
+  const Conversations = new messageChecker.Conversations;
+
+  function populateList() {
+    Conversations.initList(messages => {
+      console.log(messages);
+      let messageList = document.querySelector('.message-list');
+      messages.map(message => {
+        var messageBlock = document.createElement('div');
+        messageBlock.innerHTML = message.subject;
+        messageList.append(messageBlock);
+      })
+    });
+  }
 
   console.log('Popup loaded');
   // call object from background here
 
   function start() {
     console.log('Starting Popup ' + chrome.app.getDetails().version);
+    populateList();
   }
 
   start();
