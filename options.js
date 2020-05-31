35088e337a002bdd5e6429285d1bb837b09df315
Dennis Brown <vilesyn@gmail.com>
2020-05-31 00:57:39 -0500

    fix options

--- options.js
+++ options.js
@@ -1,20 +1,29 @@
 (function() {
   // change interval and reply list types
   window.messageChecker = window.messageChecker || {};
+  let background = chrome.extension.getBackgroundPage();
+
+  if(background) {
+    let console = background.console
+    console.log('options loaded')
+  }
 
   let Preferences = new messageChecker.Preferences;
 
-  let notificationSetting = document.getElementById('notifications');
-  Preferences.get('notifications', notes => {
-    notificationSetting.checked = notes;
-    notificationSetting.addEventListener('click', () => {
-      Preferences.set('notifications', notif.checked);
+  let notificationCheckbox = document.querySelector('input[name=notifications]');
+  Preferences.get('notifications', savedSetting => {
+    notificationCheckbox.checked = savedSetting || false;
+    console.log(notificationCheckbox.checked)
+
+    notificationCheckbox.addEventListener('change', () => {
+      console.log(notificationCheckbox.checked)
+      Preferences.set('notifications', notificationCheckbox.checked);
     });
   });
 
-  let submitButton = document.getElementById('filter-submit');
+  let submitButton = document.getElementById('filter-submit')
   submitButton.addEventListener('click', function() {
     let filterData = document.getElementById('filters');
-    Preferences.set('filter', filterData);
+    Preferences.set('filter', filterData.value);
   });
 })();
