51f5a92e0b578600b022dc7eb2e9af2d3ac661e8
Dennis Brown <vilesyn@gmail.com>
2020-05-31 15:53:16 -0500

    add new message notification support

--- background.js
+++ background.js
@@ -1,76 +1,72 @@
 (function() {
-  let nextTime, lastTime, waitTime = 0, pollTime = 15000;
+  const pollTime = 900000;
+
   const Account = new messageChecker.Account;
   const Utilities = new messageChecker.Utilities;
   const Preferences = new messageChecker.Preferences;
   const Conversations = new messageChecker.Conversations;
-  let convoMembers = messageChecker.setupConversations();
 
   function checkInbox() {
     Utilities.fetchPage(window.baseUrl, page => {
       Account.checkLoginStatus(page, loginStatus => {
         if (loginStatus.enabled) {
-          Conversations.initList(() => {
-            badgeUpdate();
-            statusUpdate();
-          });
+          Conversations.initList(list => checkList(list));
         }
       });
     });
   }
 
-  function updateList() {
-    let oldTime = waitTime;
-    if (waitTime === 0) {
-      waitTime = 15;
-    }
+  function checkList(list) {
+    let lastTime, currentTime = new Date();
 
-    if (waitTime !== oldTime) {
-      console.log('reset');
+    Preferences.get('lastTime', savedLast => {
+      lastTime = savedLast != {} ? new Date(savedLast) : currentTime;
 
-      checkTime = setInterval(updateList, pollTime);
-      lastTime = new Date();
-      nextTime = lastTime.getTime() + waitTime * 60000;
-      checkInbox();
-    }
+      Preferences.get('notifications', isEnabled => {
+        if (isEnabled) {
+          list.map(message => {
+            if (lastTime < new Date(message.time)) {
+              console.log(`new message ${message}`)
+              messageNotification(message);
+            }
+          })
+          statusUpdate(list.length);
+        }
+      })
 
-    if (nextTime !== undefined) {
-      if (Date.now() >= nextTime) {
-        console.log('timeout');
-        checkInbox();
-      }
-    }
+      Preferences.set('lastTime', new Date().valueOf());
+      badgeUpdate(list.length);
+    });
   }
 
-  function badgeUpdate() {
-    console.log('Update badge');
-    if(convoMembers.total > 0) {
-      chrome.browserAction.setBadgeText({text: convoMembers.total.toString()});
-    }
-    if(convoMembers.total !== 0) {
+  function badgeUpdate(total) {
+    console.log('badge');
+    if(total > 0) {
+      chrome.browserAction.setBadgeText({text: total.toString()});
       chrome.browserAction.setBadgeBackgroundColor({color: [185,0,0,255]});
     } else {
+      chrome.browserAction.setBadgeText({text: '0'});
       chrome.browserAction.setBadgeBackgroundColor({color: [125,125,225,255]});
     }
   }
 
-  function statusUpdate() {
-    console.log('update notification status');
-    Preferences.get('notifications', isEnabled => {
-      let total = convoMembers.total ? convoMembers.total.toString() : '0';
-      if (isEnabled && nextTime) {
-        new Notification('BlenderMarket Inbox', {
-          icon: chrome.extension.getURL('images/get_started128.png'),
-          body: `${total} messages`
-        });
-      }
+  function messageNotification(message) {
+    new Notification(`New Message for ${message.product}`, {
+      icon: chrome.extension.getURL('images/notification_icon_128.png'),
+      body: `${message.subject}`
     });
   }
 
-  checkTime = setInterval(updateList, pollTime);
-
-  function start() {
-    updateList();
+  function statusUpdate(total) {
+    if (total > 0) {
+      new Notification('BlenderMarket Inbox', {
+        icon: chrome.extension.getURL('images/notification_icon_128.png'),
+        body: `${total ? total.toString() : '0'} messages`
+      });
+    }
   }
-  start();
+
+  checkTime = setInterval(checkInbox, pollTime);
+
+  checkInbox();
 })();
